166b50a25df91038f5a7cf5755728d3111d94797
Lucas Akio Ogawa <ogawalucas@gmail.com>
2022-04-19 23:37:11 -0300

    Adding Exceptions Tests

--- src/main/java/com/ogawalucas/services/LeaseService.java
+++ src/main/java/com/ogawalucas/services/LeaseService.java
@@ -8,7 +8,11 @@ import java.time.LocalDate;
 
 public class LeaseService {
 
-    public Lease lease(User user, Movie movie) {
+    public Lease lease(User user, Movie movie) throws Exception {
+        if (movie.stock().equals(0)) {
+            throw new Exception("Movie without stock.");
+        }
+
         return new Lease(user, movie, LocalDate.now(), LocalDate.now().plusDays(1), movie.leasePrice());
     }
 }
--- src/test/java/com/ogawalucas/services/LeaseServiceTest.java
+++ src/test/java/com/ogawalucas/services/LeaseServiceTest.java
@@ -7,6 +7,7 @@ import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ErrorCollector;
+import org.junit.rules.ExpectedException;
 
 import java.time.LocalDate;
 
@@ -14,9 +15,11 @@ public class LeaseServiceTest {
 
     @Rule
     public ErrorCollector error = new ErrorCollector();
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
 
     @Test
-    public void test() {
+    public void test() throws Exception {
         // Scenery
         var user = new User("name");
         var movie = new Movie("movie", 1, 1.0);
@@ -33,5 +36,43 @@ public class LeaseServiceTest {
         error.checkThat(lease.value(), CoreMatchers.is(CoreMatchers.equalTo(1.0)));
         error.checkThat(lease.leaseDate(), CoreMatchers.is(CoreMatchers.equalTo(LocalDate.now())));
         error.checkThat(lease.returnDate(), CoreMatchers.is(CoreMatchers.equalTo(LocalDate.now().plusDays(1))));
+    }
+
+    @Test(expected = Exception.class)
+    public void testWithoutExceptionMessageValidation() throws Exception {
+        // Scenery
+        var user = new User("name");
+        var movie = new Movie("movie", 0, 1.0);
+
+        // Action
+        new LeaseService().lease(user, movie);
+    }
+
+    @Test
+    public void testWitExceptionMessageValidation() throws Exception {
+        // Scenery
+        var user = new User("name");
+        var movie = new Movie("movie", 0, 1.0);
+
+        try {
+            // Action
+            new LeaseService().lease(user, movie);
+            Assert.fail("It should throw an exception.");
+        } catch (Exception ex) {
+            // Verification
+            Assert.assertThat(ex.getMessage(), CoreMatchers.is(CoreMatchers.equalTo("Movie without stock.")));
+        }
+    }
+
+    @Test
+    public void anotherTestWitExceptionMessageValidation() throws Exception {
+        // Scenery
+        var user = new User("name");
+        var movie = new Movie("movie", 0, 1.0);
+
+        exception.expect(Exception.class);
+        exception.expectMessage("Movie without stock.");
+
+        new LeaseService().lease(user, movie);
     }
 }
